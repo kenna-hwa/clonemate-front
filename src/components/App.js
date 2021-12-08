897f7b98a76da75aa08e1f8ef5f646dfaaf70ed9
Kenna <kenna.hwa@gmail.com>
2021-12-09 00:57:57 +0900

    화면 구현 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import '../stylesheets/App.css';
+import { Button } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+    btn:{
+      main: '#f2f2f2',
+      contrastText: '#111',
+    },
+  },
+});
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <img className="main_img" src="images/todomate.jpg" />
+        <h1>todo mate</h1>
+        <h3>할 일을 작성, 계획, 관리하세요.</h3>
+      </div>
+
+      <div className="start_btn">
+        <Button className="join_btn" variant="contained" color="btn" >가입하기</Button>
+        <Button className="signin_btn" color="btn" variant="contained" >로그인</Button>
+      </div>
+
     </div>
+    </ThemeProvider>
+
   );
 }
 
